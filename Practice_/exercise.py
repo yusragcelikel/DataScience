c68026c26138d258dcd7ebaa532c98b9a6a794b3
Yusra Celikel <yusragcelikel@gmail.com>
2023-09-23 23:12:14 +0300

    Update exercise.py

--- Practice_/exercise.py
+++ Practice_/exercise.py
@@ -13,3 +13,18 @@ doubleintro = intro + intro
 
 # Print out doubleintro
 print(doubleintro)
+
+print("-------------------------------------------------")
+
+# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
