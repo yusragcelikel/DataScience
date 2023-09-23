36a1652b183732941aef0cb141361f5f8b6c3aec
Yusra Celikel <yusragcelikel@gmail.com>
2023-09-23 23:14:51 +0300

    Update exercise.py

--- Practice_/exercise.py
+++ Practice_/exercise.py
@@ -28,3 +28,8 @@ pi_string = "3.1415926"
 
 # Convert pi_string into float: pi_float
 pi_float = float(pi_string)
+
+
+print("-------------------------------------------------")
+
+print("the correct", 2)
