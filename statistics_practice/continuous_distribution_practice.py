5dd695a39bb5483fa86ae87d6d5ce1c8f380ec30
Yusra Celikel <yusragcelikel@gmail.com>
2023-10-11 17:09:29 +0300

    Update-continuous_distribution_practice

--- statistics_practice/continuous_distribution_practice.py
+++ statistics_practice/continuous_distribution_practice.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from scipy.stats import uniform
 
 np.random.seed(334)
 
 wait_times = uniform.rvs(0, 30, size=1000)
+
+plt.hist(wait_times)
+plt.show()
