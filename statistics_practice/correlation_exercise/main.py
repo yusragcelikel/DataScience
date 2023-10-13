1dc04a2a7f62acf08ffd2df445aa548810b46c6a
yusragcelikel <yusragcelikel@gmail.com>
2023-10-13 21:11:33 +0300

    correlation_practice

--- statistics_practice/correlation_exercise/main.py
+++ statistics_practice/correlation_exercise/main.py
@@ -4,12 +4,19 @@ import matplotlib.pyplot as plt
 
 
 #read the DataFrame from csv file
-world_happiness = pd.read_csv("/Users/yusragokcecelikel/Downloads/archive/2015.csv")
+world_happiness_2015 = pd.read_csv("/Users/yusragokcecelikel/Downloads/archive/2015.csv")
 
 
 #sns.scatterplot(x=life)
 
 #print(world_happiness.columns)
 
-sns.scatterplot(x = "Health (Life Expectancy)", y = "Happiness Score", data = world_happiness)
+without_trendline = sns.scatterplot(x = "Health (Life Expectancy)", y = "Happiness Score", data = world_happiness_2015)
+
+with_trendline = sns.lmplot(x = "Health (Life Expectancy)", y = "Happiness Score", data = world_happiness_2015, ci = None)
+
 plt.show()
+
+#calculate of the correlation
+cor = world_happiness_2015["Health (Life Expectancy)"].corr(world_happiness_2015["Happiness Score"])
+print(cor)
