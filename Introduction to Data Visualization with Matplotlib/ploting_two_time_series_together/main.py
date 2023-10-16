31635453f96a258c86b9c396a92f9367b49b6679
yusragcelikel <yusragcelikel@gmail.com>
2023-10-16 13:05:55 +0300

    two_time_series_practice

--- Introduction to Data Visualization with Matplotlib/ploting_two_time_series_together/main.py
+++ Introduction to Data Visualization with Matplotlib/ploting_two_time_series_together/main.py
@@ -7,12 +7,18 @@ ninety_eight = climate_change["1998-01-01" : "1998-12-31"]
 
 fig, ax = plt.subplots()
 
-#ax.plot(climate_change.index, climate_change["co2"])
-
-ax.plot(ninety_eight.index, ninety_eight["co2"])
+ax.plot(climate_change.index, climate_change["co2"])
+#ax.plot(ninety_eight.index, ninety_eight["co2"])
 
 ax.set_xlabel("Time")
 ax.set_ylabel("CO2 (ppm)")
 
+#define ax2
+ax2 = ax.twinx()
+
+ax2.plot(climate_change.index, climate_change["relative_temp"])
+
+# Since same x-axes different y-axes
+ax2.set_ylabel("Relative temperature (Celsius)")
 
 plt.show()
