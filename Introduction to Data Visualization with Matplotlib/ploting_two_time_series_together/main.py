e6283e993f8dc58f12689d80a929156bfe282f0a
yusragcelikel <yusragcelikel@gmail.com>
2023-10-16 12:57:34 +0300

    time_series_exercise

--- Introduction to Data Visualization with Matplotlib/ploting_two_time_series_together/main.py
+++ Introduction to Data Visualization with Matplotlib/ploting_two_time_series_together/main.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+# parse the date column as date and set date column as index
 climate_change = pd.read_csv("/Users/yusragokcecelikel/Downloads/CSV files/climate_change.csv", parse_dates=["date"], index_col="date")
+ninety_eight = climate_change["1998-01-01" : "1998-12-31"]
 
 fig, ax = plt.subplots()
 
-ax.plot(climate_change.index, climate_change["co2"])
+#ax.plot(climate_change.index, climate_change["co2"])
+
+ax.plot(ninety_eight.index, ninety_eight["co2"])
 
 ax.set_xlabel("Time")
 ax.set_ylabel("CO2 (ppm)")
+
 
 plt.show()
