1c45aa32d53d2acd06083c1511b96bc603537676
yusragcelikel <yusragcelikel@gmail.com>
2023-10-29 17:58:31 +0300

    Quantitative_comparisons-histograms

---
+++ Introduction to Data Visualization with Matplotlib/Quantitative comparisons and statistical visualizations/Quantitative_comparisons-histograms/main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+medalist_weights = pd.read_csv("/Users/yusragokcecelikel/Downloads/CSV files/8. Introduction to Data Visualization with Matplotlib/summer2016.csv", index_col=0)
+
+mens_rowing = medalist_weights[(medalist_weights["Sex"] == "M") & (medalist_weights["Sport"] == "Rowing")]
+mens_gymnastics = medalist_weights[(medalist_weights["Sex"] == "M") & (medalist_weights["Sport"] == "Gymnastics")]
+
+fig, ax = plt.subplots()
+
+ax.hist(mens_rowing["Height"], label="Rowing")
+ax.hist(mens_gymnastics["Height"], label="Gymnastics")
+
+ax.set_xlabel("Height (cm)")
+ax.set_ylabel("Number of observations")
+
+ax.legend()
+plt.show()
