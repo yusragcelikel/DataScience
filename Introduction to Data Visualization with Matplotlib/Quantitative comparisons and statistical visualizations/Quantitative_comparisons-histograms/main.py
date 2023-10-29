ea8fdb7109bed5d910d61b51a9815377ee753757
yusragcelikel <yusragcelikel@gmail.com>
2023-10-29 18:04:21 +0300

    histtype parameter

--- Introduction to Data Visualization with Matplotlib/Quantitative comparisons and statistical visualizations/Quantitative_comparisons-histograms/main.py
+++ Introduction to Data Visualization with Matplotlib/Quantitative comparisons and statistical visualizations/Quantitative_comparisons-histograms/main.py
@@ -8,8 +8,8 @@ mens_gymnastics = medalist_weights[(medalist_weights["Sex"] == "M") & (medalist_
 
 fig, ax = plt.subplots()
 
-ax.hist(mens_rowing["Height"], label="Rowing", bins=[150, 160, 170, 180, 190, 200, 210])
-ax.hist(mens_gymnastics["Height"], label="Gymnastics", bins=[150, 160, 170, 180, 190, 200, 210])
+ax.hist(mens_rowing["Height"], label="Rowing", bins=[150, 160, 170, 180, 190, 200, 210], histtype="step")
+ax.hist(mens_gymnastics["Height"], label="Gymnastics", bins=[150, 160, 170, 180, 190, 200, 210], histtype="step")
 
 ax.set_xlabel("Height (cm)")
 ax.set_ylabel("Number of observations")
