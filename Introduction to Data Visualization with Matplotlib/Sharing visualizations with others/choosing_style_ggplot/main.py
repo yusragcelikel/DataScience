371862171bdb6f69370820c8928f67359f8c8c35
yusragcelikel <yusragcelikel@gmail.com>
2023-11-01 20:11:30 +0300

    ggplot

---
+++ Introduction to Data Visualization with Matplotlib/Sharing visualizations with others/choosing_style_ggplot/main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+seattle_weather = pd.read_csv("/Users/yusragokcecelikel/Downloads/CSV files/8. Introduction to Data Visualization with Matplotlib/seattle_weather.csv", index_col=0)
+austin_weather = pd.read_csv("/Users/yusragokcecelikel/Downloads/CSV files/8. Introduction to Data Visualization with Matplotlib/austin_weather.csv", index_col=0)
+seattle_weather_first_twelve = seattle_weather[:12]
+
+plt.style.use("ggplot")
+
+fig, ax = plt.subplots()
+
+ax.plot(seattle_weather_first_twelve["DATE"], seattle_weather_first_twelve["MLY-TAVG-NORMAL"])
+ax.plot(austin_weather["DATE"], austin_weather["MLY-TAVG-NORMAL"])
+
+ax.set_xlabel("Time (months)")
+ax.set_ylabel("Average temperature (Fahrenheit degrees)")
+
+plt.show()
